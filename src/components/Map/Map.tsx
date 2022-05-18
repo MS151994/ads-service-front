f2bd763d66e02a4b79ea2b8d7df41669ed541cf4
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-05-18 21:11:15 +0200

    add Map to component.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,23 @@
-import "./map.css"
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet"
+import '../../utils/fix-map-icon';
+import "./map.css";
+import "leaflet/dist/leaflet.css";
 
 export const Map = () => {
     return (
         <div className="map__container">
-            map
+            <MapContainer center={[51.5954382, 18.9301508]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> & contributors"
+                />
+                <Marker position={[51.5954382, 18.9301508]}>
+                    <Popup>
+                        <h2>test</h2>
+                        <p>test p</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
