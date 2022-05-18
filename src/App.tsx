144b5b8de8593b3d09ac6c137a9adc9e75d6b2bf
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-05-18 11:53:00 +0200

    move to fixed position in header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
+import {Map} from "./components/Map/Map";
 
 export const App = () => {
     return (
         <div className="App">
             <Header/>
+            <Map/>
         </div>
     );
 }
